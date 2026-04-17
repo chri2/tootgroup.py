234871edbc632d64865a3803349fb894114ba4b4
Chris Vogel <github@z9.de>
2026-04-17 09:15:41 +0200

    resolve merge conflict

--- tootgroup.py
+++ tootgroup.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/home/tootgroup/venv/bin/python
 """tootgroup.py
 
 Andreas Schreiner
-@andi@fed.sonnenmulde.at
+@andis@chaos.social
 andreas.schreiner@sonnenmulde.at
 
 This program is free software: you can redistribute it and/or modify
@@ -105,7 +105,7 @@ def main():
     notification_count = 0
     max_notifications = 100
     max_notification_id = None
-    latest_notification_id = 0
+    latest_notification_id = str(0)
     my_notifications = []
     get_more_notifications = True
 
@@ -120,7 +120,8 @@ def main():
             if len(get_notifications) > 0:
                 latest_notification_id = get_notifications[0].id
             else:  # If there have not been any notifications yet, set value to "0"
-                latest_notification_id = 0
+                # latest_notification_id is already initialized above with the value "0"
+                # latest_notification_id = 0
                 print(
                     "Nothing to do yet! Start interacting with your group account first."
                 )
@@ -132,7 +133,7 @@ def main():
         # Initialize "last_seen_id" on first run. Notifications are ignored up
         # to this point, but newer ones will be considered subsequently.
         if my_config[group_name]["last_seen_id"] == "catch-up":
-            my_config[group_name]["last_seen_id"] = str(latest_notification_id)
+            my_config[group_name]["last_seen_id"] = latest_notification_id
             config_store["write_NEW"] = True
             print("Caught up to current timeline. Run again to start group-tooting.")
 
@@ -185,12 +186,12 @@ def main():
                         if not commandline_arguments["dry_run"]:
                             masto.status_reblog(notification.status.id)
                             print(
-                                "Retooted from notification ID: " + str(notification.id)
+                                "Retooted from notification ID: " + notification.id
                             )
                         else:
                             print(
                                 "DRY RUN - would have retooted from notification ID: "
-                                + str(notification.id)
+                                + notification.id
                             )
 
             # Is reposting of direct messages configured? - if yes then:
@@ -231,12 +232,12 @@ def main():
                             )
                             print(
                                 "Newly posted from DM with notification ID: "
-                                + str(notification.id)
+                                + notification.id
                             )
                         else:
                             print(
                                 "DRY RUN - would have newly posted from DM with notification ID: "
-                                + str(notification.id)
+                                + notification.id
                             )
 
                     # If the DM does not start with the group account's @username it will not
@@ -260,7 +261,7 @@ def main():
                             )
                             print(
                                 "Not posted from DM with notification ID: "
-                                + str(notification.id),
+                                + notification.id,
                                 end="",
                             )
                             print(
@@ -271,7 +272,7 @@ def main():
                         else:
                             print(
                                 "DRY RUN - received DM with notification ID: "
-                                + str(notification.id)
+                                + notification.id
                                 + ", but it did not begin with @"
                                 + my_account["username"]
                                 + "!"
